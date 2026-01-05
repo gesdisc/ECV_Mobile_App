a91c2381de9046df408699d751c7e8d8d275ee97
Mishel Ghukasyan <mishelghukasyan@gmail.com>
2026-01-05 12:48:35 -0800

    add 'get info' feature on catalog page

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -7,3 +7,7 @@ export function toLocalShortDateTime(str: string | number | Date): string {
 export function toStartOfDay(str: string | number | Date): string {
   return dayjs(str).startOf("day").format("YYYY-MM-DD[T]HH:mm:ss");
 }
+
+export function getDate(str: string | number | Date): string {
+  return dayjs(str).format("YYYY-MM-DD");
+}
