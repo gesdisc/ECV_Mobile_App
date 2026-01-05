93e5045c8665be265147de477647d4967c79a6b7
Mishel Ghukasyan <mishelghukasyan@gmail.com>
2026-01-05 10:45:25 -0800

    add loading spinner on catalog page

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,15 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { IonContent, IonPage } from "@ionic/react";
 import { useHistory } from "react-router-dom";
-
-import { VariableWithLabel } from "../../data/browse-variables.types";
-import { TabMenuLabels } from "../../constants/ui";
-import { getDate } from "../../utils/date";
-import { getAllData, IndexedDbStores } from "../../data/localforage";
-
-// import { fetchAndCacheCatalog } from "../../data/queryClient";
 import { useCatalogQuery } from "../../data/useCatalogQuery";
 
+import { TabMenuLabels } from "../../constants/ui";
+import { getDate } from "../../utils/date";
+
+import TerraLoader from "@nasa-terra/components/dist/react/loader";
 import Banner from "../UI/Banner";
 import Variables from "./Variables";
 import InfoPanel from "../UI/InfoPanel";
@@ -18,28 +15,10 @@ const Catalog: React.FC = () => {
   const [variableId, setVariableId] = useState("");
   const history = useHistory();
   const { data: catalog = [], isLoading, isFetching } = useCatalogQuery();
-  console.log("Catalog data:", catalog);
+
   const currentVariable = catalog.find(
     (data) => data.dataFieldId === variableId
   );
-
-  console.log("isFetching:", isFetching);
-  console.log("isLoading:", isLoading);
-
-  //   const getCatalog = async () => {
-  //   try {
-  //     setIsLoadingCatalog(true);
-
-  //     const cachedItems = await getAllData(IndexedDbStores.CATALOG);
-  //     console.log("Fetched catalog from IndexedDB:", cachedItems);
-  //     setCatalog(cachedItems);
-  //     setHydrated(true);
-  //   } catch (error) {
-  //     console.error("Error fetching catalog from IndexedDB:", error);
-  //   } finally {
-  //     setIsLoadingCatalog(false);
-  //   }
-  // };
 
   const variableChangeHandler = (variable: string) =>
     history.push(`/${TabMenuLabels.PLOT}`, variable);
@@ -105,9 +84,10 @@ const Catalog: React.FC = () => {
           afterDismiss={afterInfoPanelDismiss}
         />
         <div className="ion-padding">
-          {/* TODO: USE LOADING SPINNER */}
-          {isLoading && <p>Loading catalog...</p>}
-          {!isLoading && catalog.length === 0 ? (
+          {isLoading && (
+            <TerraLoader indeterminate variant="large"></TerraLoader>
+          )}
+          {catalog.length === 0 ? (
             <p>Couldn&apos;t Find Catalog!</p>
           ) : (
             <Variables
