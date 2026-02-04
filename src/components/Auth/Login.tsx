0dcaff8174237beee787d4382db42a3ed2c164a8
Mishel Ghukasyan <mishelghukasyan@gmail.com>
2026-02-04 09:23:54 -0800

    add uid after login, ionic popover component, custom styles for popover and terra text button

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,5 +1,8 @@
 import React, { useRef } from "react";
-import { TOKEN_KEY, useAuth } from "../../store/AuthContext";
+import { IonContent, IonIcon, IonPopover } from "@ionic/react";
+import { caretDownOutline } from "ionicons/icons";
+
+import { useAuth } from "../../store/AuthContext";
 
 import TerraLoader from "@nasa-terra/components/dist/react/loader";
 import TerraLogin, {
@@ -7,10 +10,12 @@ import TerraLogin, {
 } from "@nasa-terra/components/dist/react/login";
 import TerraButton from "@nasa-terra/components/dist/react/button";
 
+import styles from "./Login.module.css";
+
 const EDL_DOMAIN = "https://uat.urs.earthdata.nasa.gov";
 
 const Login: React.FC = () => {
-  const { login, logout } = useAuth();
+  const { login, logout, user } = useAuth();
   const hydratedRef = useRef(false);
 
   // Emitted when a bearer token has been received from EDL.
@@ -23,8 +28,6 @@ const Login: React.FC = () => {
 
     hydratedRef.current = true;
 
-    // if (localStorage.getItem(TOKEN_KEY)) return;
-
     login(token, user);
   };
 
@@ -34,22 +37,42 @@ const Login: React.FC = () => {
   };
 
   return (
-    <TerraLogin onTerraLogin={terraLoginHandler} buttonLabel="Login">
-      <TerraLoader
-        slot="loading"
-        indeterminate
-        variant="small"
-        style={{ height: "auto" }}
-      ></TerraLoader>
-      <TerraButton
-        slot="logged-in"
-        variant="primary"
-        href={`${EDL_DOMAIN}/logout?redirect_uri=${window.location.href}`}
-        onClick={logoutHandler}
+    <>
+      <TerraLogin onTerraLogin={terraLoginHandler} buttonLabel="Login">
+        <TerraLoader
+          slot="loading"
+          indeterminate
+          variant="small"
+          style={{ height: "auto" }}
+        ></TerraLoader>
+        <TerraButton
+          slot="logged-in"
+          variant="text"
+          id="popover-button"
+          className={styles.logout}
+        >
+          {user?.uid}
+          <IonIcon slot="suffix" icon={caretDownOutline} size="small"></IonIcon>
+        </TerraButton>
+      </TerraLogin>
+
+      <IonPopover
+        trigger="popover-button"
+        dismissOnSelect={true}
+        arrow={true}
+        className={styles.popover}
       >
-        Logout
-      </TerraButton>
-    </TerraLogin>
+        <IonContent class="ion-padding">
+          <TerraButton
+            variant="primary"
+            href={`${EDL_DOMAIN}/logout?redirect_uri=${window.location.href}`}
+            onClick={logoutHandler}
+          >
+            Logout
+          </TerraButton>
+        </IonContent>
+      </IonPopover>
+    </>
   );
 };
 
