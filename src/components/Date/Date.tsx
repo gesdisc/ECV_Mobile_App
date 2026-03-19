791acaa68431adc31580b5388fa27474a4e61f19
Mishel Ghukasyan <mishelghukasyan@gmail.com>
2026-03-19 10:02:09 -0700

    Format Ionic Date value when requesting update; add max value to End date

--- src/components/Date/Date.tsx
+++ src/components/Date/Date.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { IonContent, IonPage, IonGrid, IonRow, IonCol } from "@ionic/react";
 import { IonDatetime, DatetimeChangeEventDetail } from "@ionic/react";
+import dayjs from "dayjs";
 
 import { useDataParams } from "../../store/DataParamsContext";
-import { toStartOfDay } from "../../utils/date";
+import { getUTCStartOfDay } from "../../utils/date";
 import useProductDetails from "../../hooks/useProductDetails";
 
 import Banner from "../UI/Banner";
 
 import styles from "./Date.module.css";
-
 /**
  *
  * The component restricts selecting a start date that is after the end date
@@ -17,7 +17,7 @@ import styles from "./Date.module.css";
  *
  *
  */
-const Date = () => {
+const Date: React.FC = () => {
   const {
     params: ctxParams,
     staged: stagedParams,
@@ -29,13 +29,17 @@ const Date = () => {
   const beginDateUpdateHandler = (
     event: CustomEvent<DatetimeChangeEventDetail>
   ) => {
-    requestUpdateParams({ begin_time: event.detail.value as string });
+    requestUpdateParams({
+      begin_time: getUTCStartOfDay(event.detail.value as string),
+    });
   };
 
   const endDateUpdateHandler = (
     event: CustomEvent<DatetimeChangeEventDetail>
   ) => {
-    requestUpdateParams({ end_time: event.detail.value as string });
+    requestUpdateParams({
+      end_time: getUTCStartOfDay(event.detail.value as string),
+    });
   };
 
   return (
@@ -47,12 +51,10 @@ const Date = () => {
             <IonCol size="12" size-sm="6">
               <IonDatetime
                 presentation="date"
-                value={toStartOfDay(
-                  stagedParams.begin_time || ctxParams.begin_time
-                )}
+                value={stagedParams.begin_time || ctxParams.begin_time}
                 onIonChange={beginDateUpdateHandler}
                 min={selectedProductDetails?.dataProductBeginDateTime}
-                max={toStartOfDay(stagedParams.end_time || ctxParams.end_time)}
+                max={stagedParams.end_time || ctxParams.end_time}
                 style={{ width: "100%" }}
               >
                 <span slot="title">Select Start Date</span>
@@ -61,13 +63,10 @@ const Date = () => {
             <IonCol size="12" size-sm="6">
               <IonDatetime
                 presentation="date"
-                value={toStartOfDay(
-                  stagedParams.end_time || ctxParams.end_time
-                )}
+                value={stagedParams.end_time || ctxParams.end_time}
                 onIonChange={endDateUpdateHandler}
-                min={toStartOfDay(
-                  stagedParams.begin_time || ctxParams.begin_time
-                )}
+                min={stagedParams.begin_time || ctxParams.begin_time}
+                max={getUTCStartOfDay(dayjs())}
                 style={{ width: "100%" }}
               >
                 <span slot="title">Select End Date</span>
